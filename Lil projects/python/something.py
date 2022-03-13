6501f0161a8a97af6dfa2de0f15bc8faa4f7d6f1
João Afonso <74464325+joaoafonso02@users.noreply.github.com>
2022-03-13 23:19:21 +0000

    Update something.py

--- Lil projects/python/something.py
+++ Lil projects/python/something.py
@@ -246,3 +246,59 @@ for i in range(len(smth)):
 
 if smth == x: print("Capicua")
 else: print("Não é uma capicua")
+          
+  ## Search for pattern 'iii' in string 'piiig'.
+  ## All of the pattern must match, but it may appear anywhere.
+  ## On success, match.group() is matched text.
+  match = re.search(r'iii', 'piiig') # found, match.group() == "iii"
+  match = re.search(r'igs', 'piiig') # not found, match == None
+
+  ## . = any char but \n
+  match = re.search(r'..g', 'piiig') # found, match.group() == "iig"
+
+  ## \d = digit char, \w = word char
+  match = re.search(r'\d\d\d', 'p123g') # found, match.group() == "123"
+  match = re.search(r'\w\w\w', '@@abcd!!') # found, match.group() == "abc"
+         
+    ## i+ = one or more i's, as many as possible.
+  match = re.search(r'pi+', 'piiig') # found, match.group() == "piii"
+
+  ## Finds the first/leftmost solution, and within it drives the +
+  ## as far as possible (aka 'leftmost and largest').
+  ## In this example, note that it does not get to the second set of i's.
+  match = re.search(r'i+', 'piigiiii') # found, match.group() == "ii"
+
+  ## \s* = zero or more whitespace chars
+  ## Here look for 3 digits, possibly separated by whitespace.
+  match = re.search(r'\d\s*\d\s*\d', 'xx1 2   3xx') # found, match.group() == "1 2   3"
+  match = re.search(r'\d\s*\d\s*\d', 'xx12  3xx') # found, match.group() == "12  3"
+  match = re.search(r'\d\s*\d\s*\d', 'xx123xx') # found, match.group() == "123"
+
+  ## ^ = matches the start of string, so this fails:
+  match = re.search(r'^b\w+', 'foobar') # not found, match == None
+  ## but without the ^ it succeeds:
+  match = re.search(r'b\w+', 'foobar') # found, match.group() == "bar"
+          
+  str = 'purple alice-b@google.com monkey dishwasher'
+  match = re.search(r'\w+@\w+', str)
+  if match:
+    print(match.group())  ## 'b@google'
+          
+    str = 'purple alice-b@google.com monkey dishwasher'
+  match = re.search(r'([\w.-]+)@([\w.-]+)', str)
+  if match:
+    print(match.group())   ## 'alice-b@google.com' (the whole match)
+    print(match.group(1))  ## 'alice-b' (the username, group 1)
+    print(match.group(2))  ## 'google.com' (the host, group 2)
+          
+    try:
+    ## if the file does not exist or the read() encounters a low level error.
+    f = open(filename, 'rU')
+    text = f.read()
+    f.close()
+  except IOError:
+    ## Control jumps directly to here if any of the above lines throws IOError.
+    sys.stderr.write('problem reading:' + filename)
+
+          
+   
